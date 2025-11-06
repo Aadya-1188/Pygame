94776fe16e83f0c869ce814858ed745328c18e5c
Aadya-1188 <aadyapathak2509@gmail.com>
2025-11-06 20:18:08 +0530

    Add files via upload

---
+++ Projrct.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+
+display = pygame.display.set_mode((300 , 300))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+
+            pygame.quit()   
+
+        if event.type == pygame.KEYDOWN:
+
+            print("A has been pressed")
